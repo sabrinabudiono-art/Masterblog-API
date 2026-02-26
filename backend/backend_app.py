0128cd204a5d8f3a6a0080ce7af2845c6ee2ffc5
sabrinabudiono-art <sabrinabudiono@gmail.com>
2026-02-26 21:08:22 +0100

    added put method

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -16,9 +16,9 @@ def handle_posts():
         # Add the code that handles adding a new blog
         new_post = request.get_json()
         if 'title' not in new_post:
-            return jsonify("Title is missing"), 400
+            return jsonify({"message": "Title is missing"}), 400
         elif 'content' not in new_post:
-            return jsonify("Content is missing"), 400
+            return jsonify({"message": "Content is missing"}), 400
         # Set a new id
         if POSTS:
             new_id = max(post['id'] for post in POSTS) + 1
@@ -33,6 +33,40 @@ def handle_posts():
         return jsonify(new_post), 201
 
     return jsonify(POSTS)
+
+
+@app.route('/api/posts/<int:id>', methods=['DELETE'])
+def delete(id):
+    global POSTS
+    posts = [post for post in POSTS if post['id'] != id]
+    if len(posts) == len(POSTS):
+        return '', 404
+    POSTS = posts
+    successfully_deleted = {"message": f"Post with id {id} has been deleted successfully."}
+    return jsonify(successfully_deleted), 200
+
+
+@app.route('/api/posts/<int:id>', methods=['PUT'])
+def update(id):
+    global POSTS
+    updated_post = next((post for post in POSTS if post['id'] == id), None)
+    if updated_post is None:
+        return '', 404
+
+    new_post = request.get_json()
+
+    if 'title' not in new_post:
+        new_post['title'] = updated_post['title']
+    if 'content' not in new_post:
+        new_post['content'] = updated_post['content']
+    new_post['id'] = id
+
+    for i, post in enumerate(POSTS):
+        if post['id'] == id:
+            POSTS[i] = new_post
+            break
+
+    return jsonify(new_post), 200
 
 
 if __name__ == '__main__':
